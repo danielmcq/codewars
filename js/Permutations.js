910245848df500c44171470299ac11e44ec5a5fc
Daniel McQuiston <daniel.pro@dehugo.net>
2016-04-23 15:43:15 -0600

    Update Permutations problem code

--- js/Permutations.js
+++ js/Permutations.js
@@ -1,4 +1,7 @@
-/************************************
+typeof require != "undefined" ? require( "../../codewars-runner/frameworks/javascript/cw-2.js" ) : null;
+/**********************
+ * BEGIN: Description
+***********************
 In this kata you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
 
 
@@ -10,14 +13,30 @@ permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 
 
 The order of the permutations doesn't matter.
-*************************************/
-
+***********************
+* END: Description
+***********************/
 
 /**********************
  * BEGIN: Solution
 ***********************/
 function permutations(string) {
+	var ary = string.split(""),
+		out = [];
 
+	// If single character, only permutation is itself.
+	if ( ary.length === 1 )
+		out = ary;
+	// For two characters, there are 2 permutations. The string, and the string reversed.
+	else if ( ary.length === 2 )
+		out = [ ary.join(""), ary.reverse().join("") ];
+	// For any strings which are larger, recursively call this function to find permutations of remaining characters.
+	else
+		for ( var i = 0; i < ary.length; i++ ) {
+			out = out.concat( permutations( ary.slice( Math.max( i-1, 0 ), i ).concat( ary.slice( i, ary.length ) ).join("") ).map(function(x){ return ary[i]+x; }) );
+		}
+
+	return out;
 }
 /**********************
  * END: Solution
@@ -28,12 +47,12 @@ function permutations(string) {
 /**********************
  * BEGIN: Test Cases
 ***********************/
-describe('permutations', function() {
-  it('examples from description', function() {
-    Test.assertSimilar(permutations('a'), ['a']);
-    Test.assertSimilar(permutations('ab').sort(), ['ab', 'ba'].sort());
-    Test.assertSimilar(permutations('aabb').sort(), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'].sort());
-  });
+Test.describe('permutations', function() {
+	it('examples from description', function() {
+		Test.assertSimilar( permutations('a'), ['a'], "Single character test failed." );
+		Test.assertSimilar( permutations('ab').sort(), ['ab', 'ba'].sort(), "Two distinct character test failed." );
+		Test.assertSimilar( permutations('aabb').sort(), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'].sort(), "Multi-character test failed." );
+	});
 });
 /**********************
  * END: Test Cases
